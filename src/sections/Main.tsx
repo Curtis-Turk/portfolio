b68f2ab81133097258162fbe3e6880d189d083fb
Curtis Turk <curtis.c.turk@outlook.com>
2025-01-05 17:47:28 +0000

    Add title switching

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -1,6 +1,13 @@
-import { useRef, useEffect } from "react";
+import { useState } from "react";
 import { Section, scrollToSection } from "../components/SectionNav";
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
+
+enum Title {
+  NONE = " ",
+  CITRUS = "Citrus",
+  CURTIS = "Curtis",
+  RUSTIC = "Rustic",
+}
 
 interface MainProps {
   onIntersect: (isIntersecting: boolean) => void;
@@ -8,11 +15,21 @@ interface MainProps {
 
 function Main({ onIntersect }: MainProps) {
   const ref = useIntersectionObserver(onIntersect);
+  const [currentTitle, setCurrentTitle] = useState<Title>(Title.NONE);
+
+  const cycleTitle = () => {
+    const titles = Object.values(Title);
+    const currentIndex = titles.indexOf(currentTitle);
+    const nextIndex = (currentIndex + 1) % titles.length;
+    setCurrentTitle(titles[nextIndex]);
+  };
 
   return (
     <section id={Section.MAIN} ref={ref} className="section">
-      <button id="citrus">🍊</button>
-      {/* <h1 id="main-title">Citrus</h1> */}
+      <button id="citrus" onClick={cycleTitle}>
+        🍊
+      </button>
+      <h1 id="main-title">{currentTitle}</h1>
       <button className="orange" onClick={() => scrollToSection(Section.ABOUT)}>
         🟠
       </button>
