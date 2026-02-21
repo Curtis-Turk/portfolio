391336f14029553eb29b0bb821850e85f3eedd99
Curtis Turk <curtis.c.turk@outlook.com>
2026-02-21 12:10:30 +0000

    introduced router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Peat from "./pages/Peat";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  if (typeof window !== "undefined" && window.location.pathname === "/peat") {
-    return <Peat />;
-  }
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/peat" element={<Peat />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
